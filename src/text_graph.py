ce67c0f8947136c9c9f86acc21ae515199cf6589
Qiji Zhou <qiji.zhou@gmail.com>
2022-02-07 20:00:48 +0800

    update

--- src/text_graph.py
+++ src/text_graph.py
@@ -429,14 +429,15 @@ class TextGraph(nn.Module):
 
             if self.config['priors']:
                 prior_mus_expanded = torch.repeat_interleave(batch['prior_mus'], repeats=batch['bag_size'], dim=0)
-                mu_, logvar_ = mu_.sum(-2)/init_adj.size(-1), logvar_.sum(-2)/init_adj.size(-1)
+                mu_, logvar_ = mu_.sum(-2), logvar_.sum(-2)
 
                 mu_diff = prior_mus_expanded - mu_
                 kld = (-0.5 * torch.mean(torch.sum(
                     1 + 2 * logvar_ - mu_diff.pow(2) - logvar_.exp().pow(2), -1
                 ))) / node_num
             else:
-                mu_, logvar_ = mu_.sum(-2)/init_adj.size(-1), logvar_.sum(-2)/init_adj.size(-1)
+
+                mu_, logvar_ = mu_.sum(-2), logvar_.sum(-2)
                 kld = (-0.5 * torch.mean(torch.sum(
                     1 + 2 * logvar_ - mu_.pow(2) - logvar_.exp().pow(2), -1
                 ))) / node_num
