5050e8c3e2d53df707dbd2cef9e428f39144b8bf
Qiji Zhou <qiji.zhou@gmail.com>
2022-02-11 21:42:26 +0800

    Remove Constrained loss

--- src/text_graph.py
+++ src/text_graph.py
@@ -401,7 +401,8 @@ class TextGraph(nn.Module):
 
             if self.config['priors']:
                 prior_mus_expanded = torch.repeat_interleave(batch['prior_mus'], repeats=batch['bag_size'], dim=0)
-                mu_, logvar_ = mu_.sum(-2), logvar_.sum(-2)
+                prior_mus_expanded = prior_mus_expanded.unsqueeze(-2)
+                # mu_, logvar_ = mu_.sum(-2), logvar_.sum(-2)
 
                 mu_diff = prior_mus_expanded - mu_
                 kld = (-0.5 * torch.mean(torch.sum(
@@ -409,7 +410,7 @@ class TextGraph(nn.Module):
                 ))) / node_num
             else:
 
-                mu_, logvar_ = mu_.sum(-2), logvar_.sum(-2)
+                # mu_, logvar_ = mu_.sum(-2), logvar_.sum(-2)
                 kld = (-0.5 * torch.mean(torch.sum(
                     1 + 2 * logvar_ - mu_.pow(2) - logvar_.exp().pow(2), -1
                 ))) / node_num
